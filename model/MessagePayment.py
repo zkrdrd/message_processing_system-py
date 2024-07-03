fe2dc16488f8a881fe31bd47d1abce968621d0ca
dev_delphin <eduard.zakirov.96@mail.ru>
2024-07-03 22:00:18 +0500

    add errors

--- model/MessagePayment.py
+++ model/MessagePayment.py
@@ -1,10 +1,13 @@
 from model.Payment import Payment
 from model.TypeMessageVariables import TypeMessageVariables as TMP
+import inspect
 
 class MessagePayment(Payment):
     
     def __init__(self, TypeMessage, UidMessage,
                  AddresFrom, AddresTo, Amount) -> None:
+
+        TypeMessage=""
 
         validation = self.ValidateRequiredFields(TypeMessage, UidMessage)
         if validation != None:
@@ -14,18 +17,24 @@ class MessagePayment(Payment):
         Payment.SetMessagePaymentToPayment(self, TypeMessage, UidMessage,
                                            AddresFrom, AddresTo, Amount)
 
-
+    # Проверка коректности обязательных полей
     def ValidateRequiredFields(self, TypeMessage, UidMessage) -> str:
         
         if not TypeMessage:
-            return 'Fiend "TypeMessage" is empty'
+            return self.ErrFieldIsEmpty("TypeMessage")
 
         if not UidMessage:
-            return 'Field "UidMessage" is empry'
+            return self.ErrFieldIsEmpty("UidMessage")
 
         if (TypeMessage != TMP.TYPE_MESSAGE_CREATED.value and
             TypeMessage != TMP.TYPE_MESSAGE_PROCESSED.value and
             TypeMessage != TMP.TYPE_MESSAGE_CANCELED.value):
-            return 'Field "TypeMessage" is not correct'
+            return self.ErrFieldIsNotCorrect("TypeMessage")
         
-
+    # Возвращает ошибки если поле путое
+    def ErrFieldIsEmpty(self, field) -> str:
+        return (f'Field "{field}" is empry')
+    
+    # Возвращает ошибку елси поле имеет не верные данные 
+    def ErrFieldIsNotCorrect(self, field) -> str:
+        return (f'Field "{field}" is not correct')
